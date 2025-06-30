56d7bc3b7203ae22cc22aade4a7bd38077b73c94
tetnik <t.a.t.i.a.n.a.0.1.n.i.k.o@gmail.com>
2025-06-30 19:10:11 +0200

    complete implementation of "Castle Adventure" game using arrays and loops.

--- src/CastleAdventure.java
+++ src/CastleAdventure.java
@@ -5,18 +5,15 @@ public class CastleAdventure {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Нижче перша частина завдання для прикладу
-        // Там де в завданні вказані початкові дані у вигляді масиву - ініціалізуйте окремий масив
-        // Нижче обробляйте масив, додайте логіку обробки
-        // Частина 1: Увійди в Замок
         int[] secretCode = {7, 14, 28, 56, 112};
         System.out.println("Введіть 5 чисел, щоб відкрити двері замку:");
         boolean isCodeCorrect = true;
 
         for (int i = 0; i < secretCode.length; i++) {
-            int userInput = scanner.nextInt(); // питаємо в користувача чергове число
-            if (userInput != secretCode[i]) { // перевіряємо число проти i-го елемента масиву з правильним кодом
-                isCodeCorrect = false; // якщо натрапляємо на неправильно введене число - усю комбінацію вважаємо невірною
+            System.out.print(" Введи число " + (i + 1) + " : ");
+            int userInput = scanner.nextInt();
+            if (userInput != secretCode[i]) {
+                isCodeCorrect = false;
             }
         }
 
@@ -26,6 +23,59 @@ public class CastleAdventure {
             System.out.println("Невірний код. Спробуйте ще.");
         }
 
-        // Інші частини пригоди додайте тут!
+        System.out.println("Потрапивши у замок, ти бачиш чарівну дошку з числами.Розв'яжи загадку");
+        System.out.println("На дошці написано числа: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]");
+
+        int[] magicBoard = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
+        int sum = 0;
+
+        for (int number : magicBoard) {
+            sum += number;
+        }
+
+        System.out.println("Сума всіх магічних чисел: " + sum);
+        System.out.println("Вітаю!Кімната відкрилась!Ти можеш йти далі.");
+
+        System.out.println("Перед тобою з'явився Дракон!Захищайся,активувуй захист!");
+
+        int[] dragonAttacks = {8, 16, 24, 32, 40};
+        boolean allEven = true;
+
+        for (int attack : dragonAttacks) {
+            if (attack % 2 != 0) {
+                allEven = false;
+                break;
+            }
+        }
+        if (allEven) {
+            System.out.println("Атаки парні! Захист активовано.");
+        } else {
+            System.out.println("Увага!Непарна атака!");
+        }
+
+        System.out.println("Щоб знайти магічний ключ,введи своє улюблене число:");
+        int favoriteNumber = scanner.nextInt();
+        boolean found = false;
+
+        int[][] allSources = { secretCode, magicBoard, dragonAttacks };
+
+        outerLoop:
+        for (int[] source : allSources) {
+            for (int number : source) {
+                if (number == favoriteNumber) {
+                    found = true;
+                    break outerLoop;
+                }
+            }
+        }
+
+        if (found) {
+            System.out.println("Твій ключ знайдено!Ти можеш пройти далі.");
+        } else {
+            System.out.println("Треба більше магії...Спробуй ще.");
+        }
+
+        scanner.close();
+
     }
 }
